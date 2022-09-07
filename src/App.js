f20b1c2922222d70d868859a64172168c506010a
Vanessa W <73565294+Macrina1984@users.noreply.github.com>
2022-09-07 12:40:01 -0400

    first upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import About from './Components/About';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import ErrorPage from './Components/ErrorPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
